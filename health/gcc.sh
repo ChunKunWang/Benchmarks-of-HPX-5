ad46f733300eb048607e7ce4faf12332ba852a24
ChunKunWang <amos@cs.unc.edu>
2015-11-04 18:52:35 -0500

    finialize health program of OpenMP version and clean it.

---
+++ health/gcc.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm omp omp-icc
+
+/hpc_shared/apps/RHEL-6/x86_64/gcc/gcc-4.9.1/bin/gcc -lm -O2 -fopenmp -o omp health_task.c &&\
+
+/hpc/shared/apps/RHEL-6/x86_64/intel/14.0.2/bin/icc -O2 -openmp -o omp-icc health_task.c 
+
