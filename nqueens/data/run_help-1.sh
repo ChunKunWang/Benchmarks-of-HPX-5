e8d86fa971908edc11a98c6b4cd6957f89e5d5f1
ChunKunWang <amos@cs.unc.edu>
2015-11-06 17:36:14 -0500

    shell script for NQueen to test help-1 scheduler

---
+++ nqueens/data/run_help-1.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+command_path="../"
+
+command="hpx-5"
+
+input=16
+
+log[0]="log-help-hpx"
+
+echo -e "Create new log file."
+
+for i in 0 
+do
+	if [ -f ${log[i]} ]; then
+		rm ${log[i]}
+	#echo -e "Remove ${log[i]} file."
+	fi
+	touch ${log[i]}
+	#echo -e "Create a new ${log[i]} file."
+done
+
+
+echo -e "${log[0]} is processing..."
+for i in 0 2 4 6 8 10 16 20
+do
+	echo -e "\nThreshold:[$i]" >> ${log[0]}
+	for j in {0..4}
+	do
+		${command_path}${command} --hpx-wfthreshold=$i $input >> ${log[0]}
+	done
+done
+
+echo -e "Mission Complete!"
+
