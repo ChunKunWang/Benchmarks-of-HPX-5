a25de295b449d5c9dcee1ec96d09fe3e388d8845
ChunKunWang <amos@cs.unc.edu>
2015-11-20 02:43:30 -0500

    add random seed control and another quicksort version without thread level control

--- qsort/qsort_hybrid.c
+++ qsort/qsort_hybrid.c
@@ -1,15 +1,3 @@
-// =============================================================================
-//  High Performance ParalleX Library (libhpx)
-//
-//  Copyright (c) 2013-2015, Trustees of Indiana University,
-//  All rights reserved.
-//
-//  This software may be modified and distributed under the terms of the BSD
-//  license.  See the COPYING file for details.
-//
-//  This software was created at the Indiana University Center for Research in
-//  Extreme Scale Technologies (CREST).
-// =============================================================================
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
@@ -22,12 +10,29 @@
 #include "hpx/hpx.h"
 
 #define DNUM 1000000
-#define THREAD_LEVEL 100000
+#define THREAD_LEVEL 20
+
+bool RUN_HPX    = false;
+bool RUN_HPX_TL = false;
+bool RUN_CILK   = false;
+bool RUN_OPENMP = false;
+bool RUN_SEQ    = false;
+bool RUN_IN     = false;
+bool SET_SEED   = false;
+
+uint64_t SEED = 0;
 
 static void _usage(FILE *f, int error) {
-	fprintf(f, "Usage: hybrid [options] NUMBER\n"
+	fprintf(f, "Usage: hybrid [options] [-i SEED] NUMBER\n"
+			"\t-x, HPX-5 qsort (default)\n"
+			"\t-l, HPX-5 with thread level control\n"
+			"\t-c, Cilk qsort\n"
+			"\t-o, OpenMP qsort\n"
+			"\t-s, Sequential qsort\n"
+			"\t-b, Build-in qsort\n"
+			"\t-i, Set seed\n"
+			"\t-?, show HPX-5 options\n"
 			"\t-h, show help\n");
-	hpx_print_help();
 	fflush(f);
 	exit(error);
 }
@@ -45,9 +50,12 @@ int isSorted(double lyst[], int size);
 
 static hpx_action_t _main = 0;
 static hpx_action_t _parallelQuicksortHelper = 0;
+static hpx_action_t _NoTLQuicksortHelper = 0;
 //for parallel implementation
 int parallelQuicksort(double lyst[], int size, int tlevel);
 static int _parallelQuicksortHelper_action(void *threadarg, size_t size);
+int NoTLQuicksort(double lyst[], int size);
+static int _NoTLQuicksortHelper_action(void *threadarg, size_t size);
 
 struct thread_data{
 	double *lyst;
@@ -72,10 +80,13 @@ int compare_doubles (const void *a, const void *b);
    */
 static int _main_action(uint64_t *args, size_t size) {
 	hpx_time_t start;
-	srand(time(NULL)); //seed random
 	uint64_t NUM = *args;
 
+	if( SET_SEED ) srand(SEED);
+	else srand(time(NULL)); //seed random
+
 	printf("# of elements: %"PRIu64"\n", NUM);
+	printf("Generating random data... ");
 	//Want to compare sorting on the same list,
 	//so backup.
 	double *lystbck = (double *) malloc(NUM*sizeof(double));
@@ -84,54 +95,82 @@ static int _main_action(uint64_t *args, size_t size) {
 	//Populate random original/backup list.
 	for (int i = 0; i < NUM; i ++) {
 		lystbck[i] = 1.0*rand()/RAND_MAX;
+		//printf("%f ", lystbck[i]);
 	}
-	//copy list.
-	memcpy(lyst, lystbck, NUM*sizeof(double));
+	printf("finish!\n");
+
+	//HPX-5 parallel quicksort.
+	if( RUN_HPX_TL ) {
+		//copy list.
+		memcpy(lyst, lystbck, NUM*sizeof(double));
+		start = hpx_time_now();
+		NoTLQuicksort(lyst, NUM);
+		printf("HPX-5 quicksort       : %g ms.\n", hpx_time_elapsed_ms(start));
+		if (!isSorted(lyst, NUM)) {
+			printf("Oops, lyst did not get sorted by hpx-5 LT parallelQuicksort.\n");
+		}
+	}
+
+	//HPX-5 parallel quicksort with thread level control.
+	if( RUN_HPX ) {
+		//copy list.
+		memcpy(lyst, lystbck, NUM*sizeof(double));
+		start = hpx_time_now();
+		parallelQuicksort(lyst, NUM, THREAD_LEVEL);
+		printf("HPX-5 with TL control : %g ms.\n", hpx_time_elapsed_ms(start));
+		if (!isSorted(lyst, NUM)) {
+			printf("Oops, lyst did not get sorted by hpx-5 parallelQuicksort.\n");
+		}
+	}
+	
+	//Cilk quicksort, and timing
+	if( RUN_CILK ) {
+		//copy list.
+		memcpy(lyst, lystbck, NUM*sizeof(double));
+		start = hpx_time_now();
+		Cilk_quicksort(lyst, NUM);
+		printf("Cilk quicksort        : %g ms. \n", hpx_time_elapsed_ms(start));
+		if (!isSorted(lyst, NUM)) {
+			printf("Oops, lyst did not get sorted by Cilk_quicksort.\n");
+		}
+	}
+
+	//OpenMP quicksort, and timing
+	if( RUN_OPENMP ) {
+		//copy list.
+		memcpy(lyst, lystbck, NUM*sizeof(double));
+		start = hpx_time_now();
+		OMP_quicksort(lyst, NUM);
+		printf("OpenMP quicksort      : %g ms. \n", hpx_time_elapsed_ms(start));
+		if (!isSorted(lyst, NUM)) {
+			printf("Oops, lyst did not get sorted by OMP_quicksort.\n");
+		}
+	}
 
 	//Sequential quicksort, and timing
-/*	start = hpx_time_now();
-	quicksort(lyst, NUM);
-	printf("Sequential quicksort took: %g ms. \n", hpx_time_elapsed_ms(start));
-	if (!isSorted(lyst, NUM)) {
-		printf("Oops, lyst did not get sorted by quicksort.\n");
-	}
-*/
-	//OpenMP quicksort, and timing
-	start = hpx_time_now();
-	OMP_quicksort(lyst, NUM);
-	printf("OpenMP quicksort took: %g ms. \n", hpx_time_elapsed_ms(start));
-	if (!isSorted(lyst, NUM)) {
-		printf("Oops, lyst did not get sorted by OMP_quicksort.\n");
-	}
-
-	//Cilk quicksort, and timing
-	start = hpx_time_now();
-	Cilk_quicksort(lyst, NUM);
-	printf("Cilk quicksort took: %g ms. \n", hpx_time_elapsed_ms(start));
-	if (!isSorted(lyst, NUM)) {
-		printf("Oops, lyst did not get sorted by Cilk_quicksort.\n");
-	}
-
-	//Now, parallel quicksort.
-	//copy list.
-	memcpy(lyst, lystbck, NUM*sizeof(double));
-	start = hpx_time_now();
-	parallelQuicksort(lyst, NUM, THREAD_LEVEL);
-	printf("HPX-5 quicksort took: %g ms.\n", hpx_time_elapsed_ms(start));
-	if (!isSorted(lyst, NUM)) {
-		printf("Oops, lyst did not get sorted by parallelQuicksort.\n");
+	if( RUN_SEQ ) {
+		//copy list.
+		memcpy(lyst, lystbck, NUM*sizeof(double));
+		start = hpx_time_now();
+		quicksort(lyst, NUM);
+		printf("Sequential quicksort  : %g ms. \n", hpx_time_elapsed_ms(start));
+		if (!isSorted(lyst, NUM)) {
+			printf("Oops, lyst did not get sorted by quicksort.\n");
+		}
 	}
 
 	//Finally, built-in for reference:
-/*	memcpy(lyst, lystbck, NUM*sizeof(double));
-	start = hpx_time_now();
-	qsort(lyst, NUM, sizeof(double), compare_doubles);
-	//Compute time difference.
-	printf("Built-in qsort took: %g ms.\n\n", hpx_time_elapsed_ms(start));
-	if (!isSorted(lyst, NUM)) {
-		printf("Oops, lyst did not get sorted by qsort.\n");
+	if( RUN_IN ) {
+		memcpy(lyst, lystbck, NUM*sizeof(double));
+		start = hpx_time_now();
+		qsort(lyst, NUM, sizeof(double), compare_doubles);
+		printf("Built-in qsort        : %g ms.\n", hpx_time_elapsed_ms(start));
+		if (!isSorted(lyst, NUM)) {
+			printf("Oops, lyst did not get sorted by qsort.\n");
+		}
 	}
-*/
+
+	printf("\n");
 	free(lyst);
 	free(lystbck);
 	hpx_exit(HPX_SUCCESS);
@@ -145,14 +184,36 @@ int main (int argc, char *argv[])
 	}
 
 	int opt = 0;
-	srand(time(NULL)); //seed random
 	uint64_t NUM = DNUM;
 
-	while ((opt = getopt(argc, argv,  "h?")) != -1) {
+	while ((opt = getopt(argc, argv,  "h?bscoxil")) != -1) {
 		switch (opt) {
+			case 'l':
+				RUN_HPX_TL = true;
+				break;
+			case 'i':
+				SET_SEED = true;
+				break;
+			case 'b':
+				RUN_IN = true;
+				break;
+			case 's':
+				RUN_SEQ = true;
+				break;
+			case 'c':
+				RUN_CILK = true;
+				break;
+			case 'o':
+				RUN_OPENMP = true;
+				break;
+			case 'x':
+				RUN_HPX = true;
+				break;
 			case 'h':
 				_usage(stdout, EXIT_SUCCESS);
 			case '?':
+				hpx_print_help();
+				exit(EXIT_SUCCESS);
 			default:
 				_usage(stderr, EXIT_FAILURE);
 		}
@@ -169,6 +230,10 @@ int main (int argc, char *argv[])
 		case 1:
 			NUM = atoi(argv[0]);
 			break;
+		case 2:
+			SEED = atoi(argv[0]);
+			NUM = atoi(argv[1]);
+			break;
 	}
 
 	// Register the main action
@@ -176,6 +241,9 @@ int main (int argc, char *argv[])
 			HPX_POINTER, HPX_SIZE_T);
 	HPX_REGISTER_ACTION(HPX_DEFAULT, HPX_MARSHALLED, _parallelQuicksortHelper,
 			_parallelQuicksortHelper_action, HPX_POINTER, HPX_SIZE_T);
+	HPX_REGISTER_ACTION(HPX_DEFAULT, HPX_MARSHALLED, _NoTLQuicksortHelper,
+			_NoTLQuicksortHelper_action, HPX_POINTER, HPX_SIZE_T);
+
 	// Run the main action
 	int e = hpx_run(&_main, &NUM, sizeof(NUM));
 	hpx_finalize();
@@ -248,6 +316,101 @@ int partition(double *lyst, int lo, int hi)
 	}
 	swap(lyst, storeIndex, hi);
 	return storeIndex;
+}
+
+int NoTLQuicksort(double lyst[], int size)
+{
+	hpx_addr_t theThread = HPX_HERE;
+	struct thread_data td;
+	td.lyst = lyst;
+	td.low = 0;
+	td.high = size - 1;
+	td.level = 0;
+
+	//The top-level thread
+	hpx_addr_t done = hpx_lco_future_new(sizeof(uint64_t));
+	hpx_call(theThread, _NoTLQuicksortHelper, done, &td, sizeof(td));
+	hpx_lco_wait(done);
+	hpx_lco_delete(done, HPX_NULL);
+
+	return HPX_SUCCESS;
+}
+
+static int _NoTLQuicksortHelper_action(void *threadarg, size_t size)
+{
+	int mid, t;
+
+	struct thread_data *my_data;
+	my_data = (struct thread_data *) threadarg;
+
+	//fyi:
+	//printf("Thread responsible for [%d, %d], level %d.\n",
+	//my_data->low, my_data->high, my_data->level);
+
+	if (my_data->low >= my_data->high) 
+		hpx_thread_exit(HPX_SUCCESS);
+
+	//Now we partition our part of the lyst.
+	mid = partition(my_data->lyst, my_data->low, my_data->high);
+
+	//At this point, we will create threads for the
+	//left and right sides.  Must create their data args.
+	struct thread_data thread_data_array[2];
+
+	for (t = 0; t < 2; t ++) {
+		thread_data_array[t].lyst = my_data->lyst;
+		thread_data_array[t].level = my_data->level - 1;
+	}
+	thread_data_array[0].low = my_data->low;
+	if (mid > my_data->low) {
+		thread_data_array[0].high = mid-1;
+	} else {
+		thread_data_array[0].high = my_data->low;
+	}
+	if (mid < my_data->high) {
+		thread_data_array[1].low = mid+1;
+	} else {
+		thread_data_array[1].low = my_data->high;
+	}
+	thread_data_array[1].high = my_data->high;
+
+	//Now, instantiate the threads.
+	//In quicksort of course, due to the transitive property,
+	//no elements in the left and right sides of mid will have
+	//to be compared again.
+	hpx_addr_t futures[] = {
+		hpx_lco_future_new(sizeof(int)),
+		hpx_lco_future_new(sizeof(int))
+	};
+
+	hpx_addr_t threads[] = {
+		HPX_HERE,
+		HPX_HERE
+	};
+
+	int pqs[] = {
+		0,
+		0
+	};
+
+	void *addrs[] = {
+		&pqs[0],
+		&pqs[1]
+	};
+
+	int sizes[] = {
+		sizeof(int),
+		sizeof(int)
+	};
+
+	for (t = 0; t < 2; t++){
+		hpx_call(threads[t], _parallelQuicksortHelper, futures[t],
+				(void *)&thread_data_array[t], sizeof(thread_data_array[t]));
+	}
+	hpx_lco_get_all(2, futures, sizes, addrs, NULL);
+	hpx_lco_delete(futures[0], HPX_NULL);
+	hpx_lco_delete(futures[1], HPX_NULL);
+	return HPX_SUCCESS;
 }
 
 /*
@@ -365,7 +528,7 @@ int isSorted(double lyst[], int size)
 {
 	for (int i = 1; i < size; i ++) {
 		if (lyst[i] < lyst[i-1]) {
-			printf("at loc %d, %e < %e \n", i, lyst[i], lyst[i-1]);
+			printf("at loc %d, %f < %f \n", i, lyst[i], lyst[i-1]);
 			return 0;
 		}
 	}
