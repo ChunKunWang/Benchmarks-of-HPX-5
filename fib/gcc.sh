57d51cfe72265ac6fc6cf241baef1f9d2acdc5c6
ChunKunWang <amos@cs.unc.edu>
2015-12-02 16:23:34 -0500

    delete size-icc because I move it to directory size

--- fib/gcc.sh
+++ fib/gcc.sh
@@ -6,18 +6,15 @@ code_3="fib_cilk"
 exe_seq="basic"
 exe_omp="omp"
 exe_icc="omp-icc"
-size_icc="size-icc"
 exe_cilk_icc="cilk-icc"
 
-rm ${exe_seq} ${exe_omp} ${exe_icc} ${size_icc} ${exe_cilk_icc}
+rm ${exe_seq} ${exe_omp} ${exe_icc} ${exe_cilk_icc}
 
 /hpc_shared/apps/RHEL-6/x86_64/gcc/gcc-4.9.1/bin/gcc -O2 -o ${exe_seq} ${code_1}.c &&\
 
 /hpc_shared/apps/RHEL-6/x86_64/gcc/gcc-4.9.1/bin/gcc -O2 -fopenmp -o ${exe_omp} ${code_1}.c &&\
 
 /hpc/shared/apps/RHEL-6/x86_64/intel/14.0.2/bin/icc -O2 -openmp -o ${exe_icc} ${code_1}.c &&\
-
-/hpc/shared/apps/RHEL-6/x86_64/intel/14.0.2/bin/icc -O2 -openmp -o ${size-icc} ${code_2}.c &&\
 
 /hpc/shared/apps/RHEL-6/x86_64/intel/14.0.2/bin/icc -O2 -o ${exe_cilk_icc} ${code_3}.c
 
