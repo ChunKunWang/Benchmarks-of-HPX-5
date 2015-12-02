ba1f673dfcfde7f0d870c66518edadd492319b0e
ChunKunWang <amos@cs.unc.edu>
2015-12-02 16:22:31 -0500

    change compile file for cilk code

--- fib/gcc.sh
+++ fib/gcc.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
+code_1="fib_omp"
+code_2="fib_size"
+code_3="fib_cilk"
 
-rm basic omp omp-icc
+exe_seq="basic"
+exe_omp="omp"
+exe_icc="omp-icc"
+size_icc="size-icc"
+exe_cilk_icc="cilk-icc"
 
-/hpc_shared/apps/RHEL-6/x86_64/gcc/gcc-4.9.1/bin/gcc -O2 -o basic fib-0.c &&\
+rm ${exe_seq} ${exe_omp} ${exe_icc} ${size_icc} ${exe_cilk_icc}
 
-/hpc_shared/apps/RHEL-6/x86_64/gcc/gcc-4.9.1/bin/gcc -O2 -fopenmp -o omp fib-0.c &&\
+/hpc_shared/apps/RHEL-6/x86_64/gcc/gcc-4.9.1/bin/gcc -O2 -o ${exe_seq} ${code_1}.c &&\
 
-/hpc/shared/apps/RHEL-6/x86_64/intel/14.0.2/bin/icc -O2 -openmp -o omp-icc fib-0.c &&\
+/hpc_shared/apps/RHEL-6/x86_64/gcc/gcc-4.9.1/bin/gcc -O2 -fopenmp -o ${exe_omp} ${code_1}.c &&\
 
-/hpc/shared/apps/RHEL-6/x86_64/intel/14.0.2/bin/icc -O2 -openmp -o size-icc fib_size.c
+/hpc/shared/apps/RHEL-6/x86_64/intel/14.0.2/bin/icc -O2 -openmp -o ${exe_icc} ${code_1}.c &&\
+
+/hpc/shared/apps/RHEL-6/x86_64/intel/14.0.2/bin/icc -O2 -openmp -o ${size-icc} ${code_2}.c &&\
+
+/hpc/shared/apps/RHEL-6/x86_64/intel/14.0.2/bin/icc -O2 -o ${exe_cilk_icc} ${code_3}.c
 
 
