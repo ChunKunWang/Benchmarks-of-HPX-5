374262f21349a1fb67388debddb509b525d76032
ChunKunWang <amos@cs.unc.edu>
2016-03-09 10:41:14 -0500

    add -O3 optimization option

--- uts/gcc.sh
+++ uts/gcc.sh
@@ -14,13 +14,13 @@ option=""
 
 rm ${exe_seq} ${exe_omp} ${exe_icc} ${exe_cilk} ${exe_cilk_icc}
 
-/hpc_shared/apps/RHEL-6/x86_64/gcc/gcc-4.9.1/bin/gcc ${option} -O2 -lm -o ${exe_seq} ${code_1}.c ${code_2}.c &&\
+/hpc_shared/apps/RHEL-6/x86_64/gcc/gcc-4.9.1/bin/gcc ${option} -O3 -lm -o ${exe_seq} ${code_1}.c ${code_2}.c &&\
 
-/hpc_shared/apps/RHEL-6/x86_64/gcc/gcc-4.9.1/bin/gcc ${option} -O2 -lm -fopenmp -o ${exe_omp} ${code_1}.c ${code_2}.c &&\
+/hpc_shared/apps/RHEL-6/x86_64/gcc/gcc-4.9.1/bin/gcc ${option} -O3 -lm -fopenmp -o ${exe_omp} ${code_1}.c ${code_2}.c &&\
 
-/hpc/shared/apps/RHEL-6/x86_64/intel/14.0.2/bin/icc ${option} -O2 -openmp -o ${exe_icc} ${code_1}.c ${code_2}.c &&\
+/hpc/shared/apps/RHEL-6/x86_64/intel/14.0.2/bin/icc ${option} -O3 -openmp -o ${exe_icc} ${code_1}.c ${code_2}.c &&\
 
-/hpc_shared/apps/RHEL-6/x86_64/gcc/gcc-4.9.1/bin/gcc ${option} -O2 -ldl -lcilkrts -fcilkplus -o ${exe_cilk} ${code_3}.c ${code_2}.c 
+/hpc_shared/apps/RHEL-6/x86_64/gcc/gcc-4.9.1/bin/gcc ${option} -O3 -ldl -lcilkrts -fcilkplus -o ${exe_cilk} ${code_3}.c ${code_2}.c 
 
-/hpc/shared/apps/RHEL-6/x86_64/intel/14.0.2/bin/icc ${option} -O2 -ldl -lcilkrts -o ${exe_cilk_icc} ${code_3}.c ${code_2}.c 
+/hpc/shared/apps/RHEL-6/x86_64/intel/14.0.2/bin/icc ${option} -O3 -ldl -lcilkrts -o ${exe_cilk_icc} ${code_3}.c ${code_2}.c 
 
