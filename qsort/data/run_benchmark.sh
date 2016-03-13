126f8288cecb36600cb5a530307339769115ddd5
ChunKunWang <amos@cs.unc.edu>
2016-03-13 01:15:29 -0500

    exlucde ascending and descending sorted sequence

--- qsort/data/run_benchmark.sh
+++ qsort/data/run_benchmark.sh
@@ -44,29 +44,29 @@ done
 for ((j=$start; j<=$end; j*=$step))
 do
 	echo -e "\nInput:[$j]" >> ${log[0]}
-	for k in 0 1 2
+	for k in {1..10}
 	do
 		${command_path}${command} -${flag_sort} ${flag_seed} $j >> ${log[0]}  
 	done
 done
 
-for ((j=$start; j<=$end; j*=$step))
-do
-	echo -e "\nInput:[$j]" >> ${log[1]}
-	for k in 0 1 2
-	do
-		${command_path}${command} -${flag_sort} -a ${flag_seed} $j >> ${log[1]}  
-	done
-done
+#for ((j=$start; j<=$end; j*=$step))
+#do
+#	echo -e "\nInput:[$j]" >> ${log[1]}
+#	for k in 0 1 2
+#	do
+#		${command_path}${command} -${flag_sort} -a ${flag_seed} $j >> ${log[1]}  
+#	done
+#done
 
-for ((j=$start; j<=$end; j*=$step))
-do
-	echo -e "\nInput:[$j]" >> ${log[2]}
-	for k in 0 1 2
-	do
-		${command_path}${command} -${flag_sort} -r ${flag_seed} $j >> ${log[2]}  
-	done
-done
+#for ((j=$start; j<=$end; j*=$step))
+#do
+#	echo -e "\nInput:[$j]" >> ${log[2]}
+#	for k in 0 1 2
+#	do
+#		${command_path}${command} -${flag_sort} -r ${flag_seed} $j >> ${log[2]}  
+#	done
+#done
 
 echo -e "Mission Complete!"
 
