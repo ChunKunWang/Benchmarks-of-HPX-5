c6fec2098030ce6a2eafb850a848a685f61e7ce8
ChunKunWang <amos@cs.unc.edu>
2016-03-17 19:45:18 -0400

    set up reasonable experiment parameters

--- knight/data/run_benchmark.sh
+++ knight/data/run_benchmark.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 command_path="../"
 
-command[0]="omp"
-command[1]="omp-icc"
+command[0]="omp-icc"
+command[1]="cilk-icc"
 command[2]="hpx-5"
 
-log[0]="log-omp"
-log[1]="log-icc"
+log[0]="log-omp-icc"
+log[1]="log-cilk-icc"
 log[2]="log-hpx"
 
 start=3
-end=9
+end=8
 step=1
 
 echo -e "Create new log file."
@@ -30,7 +30,7 @@ do
 	for ((j=$start; j<=$end; j+=$step))
 	do
 		echo -e "\nInput:[$j]" >> ${log[i]}
-		for k in 0 1 2
+		for k in {1..10}
 		do
 			${command_path}${command[i]} $j >> ${log[i]}
 		done
